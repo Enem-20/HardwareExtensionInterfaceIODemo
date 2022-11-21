7bedb64940247400dff395c40ddb5f5568366466
sergey.zamaro <firewall606@gmail.com>
2022-11-21 17:06:56 +0300

    submodules was fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+﻿#include "HardwareExtensionModule.h"
+
+#include "LibraryProcessor.h"
+#include "ConfigLoader.h"
+
+#include <vector>
 #include <iostream>
 
+int main(int argc, char** argv) {
+	ConfigLoader::Init(argv[0]);
+	LibraryProcessor::LoadLibraries();
+
+	HardwareExtensionModule extensionModule("Head", ConfigLoader::GetConfigFromFile("configs/IOConfigDefault.json"));
 
 
-int main() {
+	while (!extensionModule.isTaskFinished()) {
+		auto analogPins = extensionModule.GetValuesFromAnalogPins();
+		std::cout << "Analog pins:" << '\n';
+		for (auto pin : analogPins) {
+			std::cout << pin.first << ":" << pin.second << '\n';
+		}
+
+		auto digitalInputPins = extensionModule.GetInputPinStates();
+
+		std::cout << "Input pins:" << '\n';
+		for (auto pin : digitalInputPins) {
+			std::cout << pin.first << ":" << pin.second << '\n';
+		}
+
+		auto digitalOutputPins = extensionModule.GetOutputPinStates();
+
+		std::cout << "Output pins:" << '\n';
+		for (auto pin : digitalOutputPins) {
+			std::cout << pin.first << ":" << pin.second << '\n';
+		}
+
+		Sleep(500);
+	}
+
 	return 0;
 }
